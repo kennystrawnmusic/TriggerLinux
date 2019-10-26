62161d0db6566895023eedc7cbfd5f3ac928b5b1
Kenneth G. Strawn <kstrawn0@saddleback.edu>
2019-10-26 00:19:25 -0700

    Fix paths

--- customize.sh
+++ customize.sh
@@ -13,7 +13,7 @@ echo "livecd" > /etc/hostname
 
 #Download config files that emerge won't install and inject them into the system
 wget -O /etc/calamares/settings.conf https://raw.githubusercontent.com/calamares/calamares/master/settings.conf
-for i in $(ls /usr/lib64/calamares/modules); do
+for i in $(ls /etc/calamares/modules); do
   wget -O /etc/calamares/modules/$i.conf https://raw.githubusercontent.com/calamares/calamares/master/src/modules/$i/$i.conf
 done
 
@@ -21,61 +21,61 @@ done
 sed -i "s/branding:.*/branding: triggerlinux/" /etc/calamares/settings.conf
 
 #Un-break partitioning
-sed -i '84,$d' /usr/lib64/calamares/modules/partition/partition.conf
-sed -i '9,38d' /usr/lib64/calamares/modules/partition/partition.conf
-sed -i "s/#\ ensureSuspendToDisk/ensureSuspendToDisk/" /usr/lib64/calamares/modules/partition/partition.conf
-sed -i "s/#\ neverCreateSwap/neverCreateSwap/" /usr/lib64/calamares/modules/partition/partition.conf
+sed -i '84,$d' /etc/calamares/modules/partition/partition.conf
+sed -i '9,38d' /etc/calamares/modules/partition/partition.conf
+sed -i "s/#\ ensureSuspendToDisk/ensureSuspendToDisk/" /etc/calamares/modules/partition/partition.conf
+sed -i "s/#\ neverCreateSwap/neverCreateSwap/" /etc/calamares/modules/partition/partition.conf
 
 #Needed in order to successfully unpack system
-sed -i '85,$d' /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
-sed -i "s/source:.*/source:\ \"\/mnt\/cdrom\/image.squashfs\"/" /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
-sed -i "s/sourcefs:.*/sourcefs:\ \"squashfs\"/" /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
-sed -i "s/destination:.*/destination:\ \"\/\"/" /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
-sed -i '12,80d' /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
-sed -i '1,10d' /usr/lib64/calamares/modules/unpackfs/unpackfs.conf
+sed -i '85,$d' /etc/calamares/modules/unpackfs/unpackfs.conf
+sed -i "s/source:.*/source:\ \"\/mnt\/cdrom\/image.squashfs\"/" /etc/calamares/modules/unpackfs/unpackfs.conf
+sed -i "s/sourcefs:.*/sourcefs:\ \"squashfs\"/" /etc/calamares/modules/unpackfs/unpackfs.conf
+sed -i "s/destination:.*/destination:\ \"\/\"/" /etc/calamares/modules/unpackfs/unpackfs.conf
+sed -i '12,80d' /etc/calamares/modules/unpackfs/unpackfs.conf
+sed -i '1,10d' /etc/calamares/modules/unpackfs/unpackfs.conf
 
 #Add the proper number of groups
-sed -i '23a\ \ \ \ \-\ cdrom' /usr/lib64/calamares/modules/users/users.conf
-sed -i '24a\ \ \ \ \-\ cdrw' /usr/lib64/calamares/modules/users/users.conf
-sed -i '25a\ \ \ \ \-\ usb' /usr/lib64/calamares/modules/users/users.conf
-sed -i '26a\ \ \ \ \-\ plugdev' /usr/lib64/calamares/modules/users/users.conf
-sed -i '27a\ \ \ \ \-\ vboxusers' /usr/lib64/calamares/modules/users/users.conf
-sed -i '28a\ \ \ \ \-\ vboxsf' /usr/lib64/calamares/modules/users/users.conf
-sed -i '29a\ \ \ \ \-\ vboxguest' /usr/lib64/calamares/modules/users/users.conf
-sed -i '30a\ \ \ \ \-\ portage' /usr/lib64/calamares/modules/users/users.conf
-sed -i '31a\ \ \ \ \-\ messagebus' /usr/lib64/calamares/modules/users/users.conf
-sed -i '32a\ \ \ \ \-\ polkituser' /usr/lib64/calamares/modules/users/users.conf
-sed -i '33a\ \ \ \ \-\ games' /usr/lib64/calamares/modules/users/users.conf
-sed -i '34a\ \ \ \ \-\ scanner' /usr/lib64/calamares/modules/users/users.conf
-sed -i '35a\ \ \ \ \-\ bumblebee' /usr/lib64/calamares/modules/users/users.conf
-sed -i '36a\ \ \ \ \-\ lpadmin' /usr/lib64/calamares/modules/users/users.conf
+sed -i '23a\ \ \ \ \-\ cdrom' /etc/calamares/modules/users/users.conf
+sed -i '24a\ \ \ \ \-\ cdrw' /etc/calamares/modules/users/users.conf
+sed -i '25a\ \ \ \ \-\ usb' /etc/calamares/modules/users/users.conf
+sed -i '26a\ \ \ \ \-\ plugdev' /etc/calamares/modules/users/users.conf
+sed -i '27a\ \ \ \ \-\ vboxusers' /etc/calamares/modules/users/users.conf
+sed -i '28a\ \ \ \ \-\ vboxsf' /etc/calamares/modules/users/users.conf
+sed -i '29a\ \ \ \ \-\ vboxguest' /etc/calamares/modules/users/users.conf
+sed -i '30a\ \ \ \ \-\ portage' /etc/calamares/modules/users/users.conf
+sed -i '31a\ \ \ \ \-\ messagebus' /etc/calamares/modules/users/users.conf
+sed -i '32a\ \ \ \ \-\ polkituser' /etc/calamares/modules/users/users.conf
+sed -i '33a\ \ \ \ \-\ games' /etc/calamares/modules/users/users.conf
+sed -i '34a\ \ \ \ \-\ scanner' /etc/calamares/modules/users/users.conf
+sed -i '35a\ \ \ \ \-\ bumblebee' /etc/calamares/modules/users/users.conf
+sed -i '36a\ \ \ \ \-\ lpadmin' /etc/calamares/modules/users/users.conf
 
 #Calamares bootloader config
-sed -i "s/kernel:.*/kernel:\ \"\/boot\/$kernel/\"" /usr/lib64/calamares/modules/bootloader/bootloader.conf
-sed -i "s/img:.*/img:\ \"\/boot\/$initramfs/\"" /usr/lib64/calamares/modules/bootloader/bootloader.conf
-sed -i "s/fallback:.*/fallback: \"\/boot\/$initramfs/\"" /usr/lib64/calamares/modules/bootloader/bootloader.conf
-sed -i "s/timeout:.*/timeout:\ \"0\"/" /usr/lib64/calamares/modules/bootloader/bootloader.conf
+sed -i "s/kernel:.*/kernel:\ \"\/boot\/$kernel/\"" /etc/calamares/modules/bootloader/bootloader.conf
+sed -i "s/img:.*/img:\ \"\/boot\/$initramfs/\"" /etc/calamares/modules/bootloader/bootloader.conf
+sed -i "s/fallback:.*/fallback: \"\/boot\/$initramfs/\"" /etc/calamares/modules/bootloader/bootloader.conf
+sed -i "s/timeout:.*/timeout:\ \"0\"/" /etc/calamares/modules/bootloader/bootloader.conf
 
 #Integrate calamares with Portage
-sed -i "s/backend:.*/backend:\ portage/" /usr/lib64/calamares/modules/packages/packages.conf
-sed -i "s/update_system:.*/update_system:\ true/" /usr/lib64/calamares/modules/packages/packages.conf
+sed -i "s/backend:.*/backend:\ portage/" /etc/calamares/modules/packages/packages.conf
+sed -i "s/update_system:.*/update_system:\ true/" /etc/calamares/modules/packages/packages.conf
 
 #Don't need calamares on the system post-installation
-sed -i "162,166d" /usr/lib64/calamares/modules/packages/packages.conf
-sed -i '$d' /usr/lib64/calamares/modules/packages/packages.conf
-sed -i '$d' /usr/lib64/calamares/modules/packages/packages.conf
-sed -i '$d' /usr/lib64/calamares/modules/packages/packages.conf
-sed -i '$a\ \ \ \ \-\ app-admin\/calamares' /usr/lib64/calamares/modules/packages/packages.conf
+sed -i "162,166d" /etc/calamares/modules/packages/packages.conf
+sed -i '$d' /etc/calamares/modules/packages/packages.conf
+sed -i '$d' /etc/calamares/modules/packages/packages.conf
+sed -i '$d' /etc/calamares/modules/packages/packages.conf
+sed -i '$a\ \ \ \ \-\ app-admin\/calamares' /etc/calamares/modules/packages/packages.conf
 
 #Calamares Plymouth theme
-sed -i "s/plymouth_theme:.*/plymouth_theme: bgrt/" /usr/lib64/calamares/modules/plymouthcfg/plymouthcfg.conf
+sed -i "s/plymouth_theme:.*/plymouth_theme: bgrt/" /etc/calamares/modules/plymouthcfg/plymouthcfg.conf
 
 #Default calamares locale
-sed -i "s/zone:.*/zone:\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \"Los_Angeles\"/" /usr/lib64/calamares/modules/locale/locale.conf
-sed -i "s/\ \ \ \ selector:\ /selector:\ \"time_zone\"" /usr/lib64/calamares/modules/locale/locale.conf
+sed -i "s/zone:.*/zone:\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \"Los_Angeles\"/" /etc/calamares/modules/locale/locale.conf
+sed -i "s/\ \ \ \ selector:\ /selector:\ \"time_zone\"" /etc/calamares/modules/locale/locale.conf
 
 #Reboot immediately when finished
-sed -i "s/restartNowCommand:.*/restartNowCommand: \"echo b > /proc/sysrq-trigger\"/" /usr/lib64/calamares/modules/finished/finished.conf
+sed -i "s/restartNowCommand:.*/restartNowCommand: \"echo b > /proc/sysrq-trigger\"/" /etc/calamares/modules/finished/finished.conf
 
 #Enable "packages" module
 sed -i "124a\ \ \-\ packages" /etc/calamares/settings.conf
@@ -94,7 +94,7 @@ echo "shopt -s extglob" >> /etc/profile
 #GRUB Plymouth entry
 sed -i "s/.*GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash zram.num_devices=1\"/" /etc/default/grub
 sed -i "s/.*GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/" /etc/default/grub
-sed -i "s/GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/" /usr/lib64/calamares/modules/grubcfg/grubcfg.conf
+sed -i "s/GRUB_TIMEOUT.*/GRUB_TIMEOUT=0/" /etc/calamares/modules/grubcfg/grubcfg.conf
 
 #Root password
 echo -e "triggerlinux\ntriggerlinux" | passwd
@@ -112,14 +112,14 @@ echo -e "panel-size=40" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschem
 
 #Desktop Background
 echo -e "[org.gnome.desktop.background:GNOME]" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
-echo -e "picture-options='stretched'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
+echo -e "picture-options='str/etched'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "picture-uri='file:///usr/share/backgrounds/gnome/SeaSunset.jpg'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "primary-color='#ffffff'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "secondary-color='#000000'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 
 #Lock screen background
 echo -e "[org.gnome.desktop.screensaver:GNOME]" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
-echo -e "picture-options='stretched'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
+echo -e "picture-options='str/etched'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "picture-uri='file:///usr/share/backgrounds/gnome/SeaSunset.jpg'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "primary-color='#ffffff'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
 echo -e "secondary-color='#000000'" >> /usr/share/glib-2.0/schemas/00_org.gnome.shell.gschema.override
