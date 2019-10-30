2204d2b7e031f6b57ed1052f7011d5e465b42189
Kenneth G. Strawn <kstrawn0@saddleback.edu>
2019-10-29 23:22:29 -0700

    Configure bootstrap script to emerge this tree instead of copying it

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+#!/usr/bin/sudo /bin/bash
 
 remotefilename=$(wget -O - http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/ | grep -Eo "stage3-amd64-systemd-[0-9]{1,}.tar.bz2" | head -n1)
 url=http://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-systemd/$remotefilename
-
-if [ $ -ne 0 ]; then
-  echo "Please run this script as root or (if installed and configured) with sudo prepended"
-fi
 
 echo "Welcome to the TriggerLinux Gentoo Edition bootstrap script!"
 
@@ -20,6 +16,7 @@ echo "Copying portage_confdir to the chroot"
 rm -rf /tmp/triggerlinux-chroot/etc/portage
 cp -r portage /tmp/triggerlinux-chroot/etc/portage
 rm -rf /tmp/triggerlinux-chroot/var/db/repos/gentoo
+mkdir -p /tmp/triggerlinux-chroot/var/db/repos/gentoo
 
 echo "Binding DNS resolver configuration files to the chroot"
 mount --bind /etc/resolv.conf /tmp/triggerlinux-chroot/etc/resolv.conf
@@ -31,16 +28,19 @@ chroot /tmp/triggerlinux-chroot mount -t devtmpfs none /dev
 chroot /tmp/triggerlinux-chroot mount -t devpts none /dev/pts
 chroot /tmp/triggerlinux-chroot mount -t tmpfs none /dev/shm
 chroot /tmp/triggerlinux-chroot emerge --sync
-chroot /tmp/triggerlinux-chroot emerge app-portage/layman app-admin/sudo dev-vcs/git net-misc/wget dev-util/catalyst
+chroot /tmp/triggerlinux-chroot emerge app-portage/layman
+chroot /tmp/triggerlinux-chroot layman -L
+chroot /tmp/triggerlinux-chroot bash -c "yes | layman -a brave-overlay"
+chroot /tmp/triggerlinux-chroot bash -c "yes | layman -o https://raw.githubusercontent.com/realKennyStrawn93/triggerlinux-overlay/master/triggerlinux-overlay.xml -f -a triggerlinux-overlay"
 
 echo "Cloning git repo inside the chroot"
-chroot /tmp/triggerlinux-chroot bash -c "git clone https://github.com/realKennyStrawn93/TriggerLinux.git /tmp/triggerlinux"
+chroot /tmp/triggerlinux-chroot emerge dev-util/triggerlinux-autobuilder
 
 echo "Running autocatalyst.sh inside the chroot"
-chroot /tmp/triggerlinux-chroot bash -c "cd /tmp/triggerlinux && ./autocatalyst.sh && cd /"
+chroot /tmp/triggerlinux-chroot bash -c "cd /opt/TriggerLinux && ./autocatalyst.sh && cd /"
 
 echo "Copying ISO image out of the chroot"
-cp -r /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default/*.iso /home/triggerlinux-livecd-$(date +%Y%m%d)-x86_64.iso
+cp -r /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default/*.iso ./triggerlinux-livedvd-$(date +%Y%m%d)-x86_64.iso
 
 echo "Removing the chroot"
 umount -lf /tmp/triggerlinux-chroot/{etc/resolv.conf,dev/shm,dev/pts,dev,sys,proc}
