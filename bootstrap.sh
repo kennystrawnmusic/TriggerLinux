9d8ceda464cd5727ac86e4485c890a107331bf33
Kenny Strawn <Kenny.Strawn@gmail.com>
2019-11-02 13:47:42 -0700

    Make bootstrapping more time-efficient

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,10 @@ echo "Extracting stage tarball as chroot"
 mkdir -p /tmp/triggerlinux-chroot
 tar -C /tmp/triggerlinux-chroot -xjvf /tmp/chroot.tar.bz2
 
+echo "Moving the stage tarball into the chroot"
+mkdir -p /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default
+mv /tmp/chroot.tar.bz2 /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default/livecd-stage1-amd64-installer-latest.tar.bz2
+
 echo "Copying portage_confdir to the chroot"
 rm -rf /tmp/triggerlinux-chroot/etc/portage
 cp -r portage /tmp/triggerlinux-chroot/etc/portage
@@ -35,13 +39,13 @@ chroot /tmp/triggerlinux-chroot bash -c "yes | layman -o https://raw.githubuserc
 # Needed to avoid "permission denied" errors when building
 chroot /tmp/triggerlinux-chroot chmod -R 777 /var/cache
 
-echo "Cloning git repo inside the chroot"
+echo "Instaling the TriggerLinux build scripts inside the chroot"
 chroot /tmp/triggerlinux-chroot emerge dev-util/triggerlinux-autobuilder
 
 echo "Running autocatalyst.sh inside the chroot"
 chroot /tmp/triggerlinux-chroot bash -c "cd /opt/TriggerLinux && ./autocatalyst.sh && cd /"
 
-echo "Copying ISO image out of the chroot"
+echo "Copying ISO image and stage tarball out of the chroot"
 cp /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default/*.tar.bz2 $scriptdir/triggerlinux-livedvd-stage1-latest.tar.bz2
 cp /tmp/triggerlinux-chroot/var/tmp/catalyst/builds/default/*.iso $scriptdir/triggerlinux-livedvd-latest-x86_64.iso
 
