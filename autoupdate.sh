95a8786425fd8790206d27f5d818a03c3dc4831a
Kenny Strawn <Kenny.Strawn@gmail.com>
2019-11-04 06:48:34 -0800

    Check AppImage versions in autoupdate script before doing anything

--- autoupdate.sh
+++ autoupdate.sh
@@ -8,29 +8,52 @@ appimagelauncher_var2=$(wget -qO - $appimagelauncher_base/$appimagelauncher_var1
 appimagelauncher=$appimagelauncher_base/$(wget -qO - https://artifacts.assassinate-you.net/artifactory/AppImageLauncher | grep -Eo "travis-[0-9]{1,}" | sort -rn | head -n1)/$appimagelauncher_var2
 
 #AppImage Daemon
-wget -O /usr/bin/appimaged https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage
-chmod a+x /usr/bin/appimaged
-wget -O /lib/systemd/system/appimaged.service https://raw.githubusercontent.com/AppImage/appimaged/master/resources/appimaged.service
+current_version=$(appimaged --version)
+wget -O /tmp/appimaged https://github.com/AppImage/appimaged/releases/download/continuous/appimaged-x86_64.AppImage
+new_version=$(/tmp/appimaged --version)
+if [ "$new_version" == "$current_version" ]; then
+  echo "appimaged already up-to-date; cleaning up"
+  rm -f /tmp/appimaged
+else
+  cat /tmp/appimaged > /usr/bin/appimaged
+  chmod a+x /usr/bin/appimaged
+  wget -O /lib/systemd/system/appimaged.service https://raw.githubusercontent.com/AppImage/appimaged/master/resources/appimaged.service
 systemctl enable appimaged.service
+fi
 
 #AppImage Updater
-wget -O /usr/bin/AppImageUpdate https://github.com/AppImage/AppImageUpdate/releases/download/continuous/AppImageUpdate-x86_64.AppImage
-chmod a+x /usr/bin/AppImageUpdate
+current_version=$(AppImageUpdate --version)
+wget -O /tmp/AppImageUpdate https://github.com/AppImage/AppImageUpdate/releases/download/continuous/AppImageUpdate-x86_64.AppImage
+new_version=$(/tmp/AppImageUpdate --version)
+if [ "$new_version" == "$current_version" ]; then
+  echo "AppImageUpdate already up-to-date; cleaning up"
+  rm -f /tmp/AppImageUpdate
+else
+  cat /tmp/AppImageUpdate > /usr/bin/AppImageUpdate
+  chmod a+x /usr/bin/AppImageUpdate
 wget -O /usr/share/applications/AppImageUpdate.desktop https://raw.githubusercontent.com/AppImage/AppImageUpdate/rewrite/resources/AppImageUpdate.desktop
 
 #AppImageLauncher
-wget -O /usr/bin/appimagelauncher-lite $appimagelauncher
-chmod a+x /usr/bin/appimagelauncher-lite
-appimagelauncher-lite --install
-appimagelauncher-lite --appimage-extract
-cp squashfs-root/usr/share/applications/appimagelauncher-lite.desktop /usr/share/applications/appimagelauncher-lite.desktop
-for i in $(ls /usr/share/icons); do
-  if [ ! -d /usr/share/icons/$i/192x192 ]; then
-    mkdir -p /usr/share/icons/$i/192x192/apps
-  fi
-  cp squashfs-root/usr/share/icons/hicolor/192x192/apps/AppImageLauncher.png /usr/share/icons/$i/192x192/apps/AppImageLauncher.png
-done
-rm -rf squashfs-root
+current_version=$(appimagelauncher-lite --version)
+wget -O /tmp/appimagelauncher-lite $appimagelauncher
+chmod a+x /tmp/appimagelauncher-lite
+new_version=$(/tmp/appimagelauncher-lite --version)
+if [ "$new_version" == "$current_version" ]; then
+  echo "AppImageLauncher already up-to-date; cleaning up"
+  rm -f /tmp/appimagelauncher-lite
+else
+  cd /tmp
+  cat /tmp/appimagelauncher-lite > /usr/bin/appimagelauncher-lite
+  appimagelauncher-lite --appimage-extract
+  cp squashfs-root/usr/share/applications/appimagelauncher-lite.desktop /usr/share/applications/appimagelauncher-lite.desktop
+  for i in $(ls /usr/share/icons); do
+    if [ ! -d /usr/share/icons/$i/192x192 ]; then
+      mkdir -p /usr/share/icons/$i/192x192/apps
+    fi
+    cp squashfs-root/usr/share/icons/hicolor/192x192/apps/AppImageLauncher.png /usr/share/icons/$i/192x192/apps/AppImageLauncher.png
+  done
+  rm -rf squashfs-root
+fi
 
 #Update Kdenlive AppImage
 current_version=$(/Applications/Kdenlive.AppImage --version)
