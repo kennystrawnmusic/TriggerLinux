54d6511a9604496bfe1a142cd108f0a60694cce3
Kenny Strawn <Kenny.Strawn@gmail.com>
2019-11-04 21:17:34 -0800

    Fix audio

--- autocatalyst.sh
+++ autocatalyst.sh
@@ -130,7 +130,8 @@ build() {
   install -m 755 $scriptdir/imgmerge $stage1chroot/usr/bin/imgmerge && \
   cat $scriptdir/os-release > $stage1chroot/etc/os-release && \
   cat $scriptdir/grubrc > $stage1chroot/etc/default/grub && \
-  for i in $scriptdir/grub.d/*; do cat $i > $stage1chroot/etc/grub.d/$(echo $i | cut -d\/ -f14); done && \
+  rm -rf $stage1chroot/etc/grub.d && \
+  cp -r grub.d $stage1chroot/etc/ && \
   cp $scriptdir/isobuild.desktop $stage1chroot/usr/share/applications/isobuild.desktop && \
   catalyst -f $stage2spec
 }
